3741952b2f4564f7847c69c4ca39d12a1b5ec5e9
DanielRJohansen <drdj@hotmail.dk>
2021-08-27 23:58:14 +0200

    Work on Blocks KdTree

--- Quantom/Quantom/Engine.cuh
+++ Quantom/Quantom/Engine.cuh
@@ -18,6 +18,7 @@ private:
 	Simulation* simulation;
 	void fillBox();
 	void initBlocks();
+
 	void placeBody(SimBody* body);
 
 
@@ -27,10 +28,10 @@ private:
 	// Helper functions
 	Int3 posToBlockIndex(Double3* pos) {
 		return Int3(
-			pos->x + simulation->box_size / 2) / simulation->box_size * simulation->blocks_per_dim,
-			pos->y + simulation->box_size / 2) / simulation->box_size * simulation->blocks_per_dim,
-			pos->z + simulation->box_size / 2) / simulation->box_size * simulation->blocks_per_dim
-		)
+			(pos->x + simulation->box_size / 2) / simulation->box_size * simulation->blocks_per_dim,
+			(pos->y + simulation->box_size / 2) / simulation->box_size * simulation->blocks_per_dim,
+			(pos->z + simulation->box_size / 2) / simulation->box_size * simulation->blocks_per_dim
+			);
 	}
 
 
--- Quantom/Quantom/QuantomTypes.cuh
+++ Quantom/Quantom/QuantomTypes.cuh
@@ -71,3 +71,129 @@ public:
 		blocks = blocks_temp;
 	}	
 };
+
+
+
+
+
+
+
+
+class KdTree {
+	class Node;
+	void swap(Node parent, Node child);
+
+
+
+public:
+	KdTree(){}
+	KdTree(Double3 block_center) : block_center(block_center) {}
+
+	void addNode(SimBody* body) {
+		double pos[3] = { body->pos.x, body->pos.y,	body->pos.z };
+
+		if (root == NULL)
+			root = new Node(body, NULL);
+		else
+			root->addNode(body, pos);
+	}
+
+	void balance() {
+		if (root != NULL) {
+			root->rootBalance(block_center);
+
+		}
+	}
+
+private:
+	Node* root = NULL;
+	Double3 block_center;
+
+
+	// Helper functions
+	static void swap(Node* parent, Node* child) {
+		Node* temp = parent;
+		parent = child;
+		parent->dim = child->dim;
+
+		child = temp;
+		child->dim = temp->dim;
+	}
+
+	// Structs
+
+	struct Dim {
+		Dim(){}
+		Dim(uint8_t val) : val(val) {}
+		uint8_t val = 0;
+		Dim next() {
+			if (val == 2)
+				return Dim(0);
+			return Dim(val + 1);
+		}
+	};
+
+
+	class Node {
+	public:
+		Node() {}
+		Node(SimBody* body, Node* parent, double pos[3]) : body(body), parent(parent) {
+			for (int i = 0; i < 3; i++)
+				this->pos[i] = pos[i];
+			dim = parent->dim.next();
+		}
+		Dim dim;
+
+		void addNode(SimBody* body, double* pos) {
+			if (pos[dim.val] > this->pos[dim.val]) {
+				if (right == NULL)
+					right = new Node(body, this, pos);
+				else
+					right->addNode(body, pos);
+			}
+			else {
+				if (left == NULL)
+					left = new Node(body, this, pos);
+				else
+					left->addNode(body, pos);
+			}
+		}
+
+		void rootBalance(Double3 block_center) {
+			double left_dist = 99999;
+			double right_dist = 99999;
+			double root_dist = (body->pos - block_center).len();
+			if (left != NULL)
+				left_dist = (left->body->pos - block_center).len();
+			if (right != NULL)
+				right_dist = (right->body->pos - block_center).len();
+
+			if (left_dist < right_dist) {
+				if (left_dist < root_dist)
+					swap(left, this);
+			}
+			else {
+				if (right_dist < root_dist)
+					swap(right, this);
+			}
+				
+
+
+			if (left != NULL)
+				left->nodeBalance();
+			if (right != NULL)
+				right->nodeBalance();
+		}
+		void nodeBalance();
+
+
+	private:
+		double pos[3];
+		SimBody* body;
+		Node* parent;
+		Node* right = NULL;
+		Node* left = NULL;
+
+	};
+};
+
