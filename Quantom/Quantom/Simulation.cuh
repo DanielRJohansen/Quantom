08ce15354d594ac286f6a9dcbef96b9a34871b2e
DanielRJohansen <drdj@hotmail.dk>
2022-03-08 22:19:20 +0100

    Last commit before new branch

--- Quantom/Quantom/Simulation.cuh
+++ Quantom/Quantom/Simulation.cuh
@@ -5,7 +5,7 @@
 
 
 
-constexpr double BOX_LEN = 6.f;		// Must be > twice the len of largest compound
+constexpr double BOX_LEN = 8.f;		// Must be > twice the len of largest compound
 constexpr double BOX_LEN_HALF = BOX_LEN/2.f;
 
 
@@ -56,7 +56,7 @@ const int THREADS_PER_COMPOUNDBLOCK = 128; // Must be >= max comp particles
 const int N_LIPID_COPIES = 32;
 
 
-const int SIMULATION_STEPS = 20000;
+const int SIMULATION_STEPS = 200000;
 
 //constexpr double SOLVENT_MASS = 18.01528f * 1e-3;	// kg/mol
 constexpr double SOLVENT_MASS = 12.0107 * 1e-3;	// kg/mol
